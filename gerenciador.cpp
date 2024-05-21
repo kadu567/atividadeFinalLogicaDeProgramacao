5a3ea9f33c1246c5bc2e12538bf1844dee9eda2c
kadu567 <162385570+kadu567@users.noreply.github.com>
2024-05-21 00:39:04 +0000

    feito

--- gerenciador.cpp
+++ gerenciador.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
 using namespace std;
-int id[999];
-int titulo[999];
-int descricao[999];
-int data[999];
-int status[999];
+
 int menu;
+int id[1000];
+string titulo[1000];
+string descricao[1000];
+string data[1000];
+string status[1000];
+int controle = 0;
+
 int main(){
 
+do
+{
+  
     cout << "1. Adicionar tarefa" << endl;
     cout << "2. Visualizar tarefas" << endl; 
     cout << "3. Editar tarefa" << endl; 
@@ -18,19 +26,24 @@ int main(){
     cin >> menu;
 
     if (menu == 1){
-        cout << "ID: " << endl;
-        cin.ignore(); 
-        getline (cin, id[999]);
-        cout << "Título: " << endl;
-        cin.ignore();
-        getline (cin, titulo[999]);
+        cout << "ID: ";
+        cin >> id[1000]; 
+        cout << "Título: ";
+        cin.ignore(); getline(cin,titulo[1000]);
         cout << "Descrição: " << endl;
-        cin >> descricao[999];
+        cin.ignore(); getline(cin,descricao[1000]);
         cout << "Data limite: " << endl;
-        cin >> data[999];
+        cin.ignore(); getline(cin,data[1000]);
         cout << "Status: " << endl;
-        cin >> status[999];
+        cin.ignore(); getline(cin,status[1000]);
+    
+    controle++;
     }
+    
+    if(menu == 2){
+    for (int i = 0; i < controle; i++)
+}
+        } while (menu != 0);
 
 return 0;
 }
