e4539e2f7c8147c2f1e3df797ca685ceffabfae4
kadu567 <162385570+kadu567@users.noreply.github.com>
2024-05-28 00:38:06 +0000

    a

--- gerenciador.cpp
+++ gerenciador.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int menu;
-int id[1000];
-string titulo[1000];
-string descricao[1000];
-string data[1000];
-string status[1000];
+int ID[1000];
+string Titulo[1000];
+string Descricao[1000];
+string Data[1000];
+string Status[1000];
 int controle = 0;
 
 int main(){
@@ -25,25 +25,73 @@ do
     cout << "0. Sair" << endl;
     cin >> menu;
 
+   
+   
     if (menu == 1){
-        cout << "ID: ";
-        cin >> id[1000]; 
-        cout << "Título: ";
-        cin.ignore(); getline(cin,titulo[1000]);
+        ID [controle] = controle; 
+        cout << "Título: " << endl;
+        cin.ignore(); getline(cin,Titulo[controle]);
         cout << "Descrição: " << endl;
-        cin.ignore(); getline(cin,descricao[1000]);
+        cin >> Descricao[controle];
         cout << "Data limite: " << endl;
-        cin.ignore(); getline(cin,data[1000]);
+        cin.ignore(); getline(cin,Data[controle]);
         cout << "Status: " << endl;
-        cin.ignore(); getline(cin,status[1000]);
+        cin >> Status[controle];
     
     controle++;
     }
     
     if(menu == 2){
-    for (int i = 0; i < controle; i++)
-}
-        } while (menu != 0);
+    cout << "--Lista de tarefas--" << endl;
+    for (int i = 0; i < controle; i++){;
+    cout << "ID: " << ID[i] << endl;
+    cout << "Titulo: " << Titulo[i] << endl;
+    cout << "Descricao: " << Descricao[i] << endl;
+    cout << "Data Limite: " << Data[i] << endl;
+    cout << "Status: "<< Status[i] << endl;
+        } 
+    }
 
+    if(menu == 3){
+    cout << "--Editar tarefa--" << endl;
+    
+    cout << "Tarefas disponíveis para edição:" << endl;
+    for (int i = 0; i < controle; i++){
+        cout << "ID: " << ID[i] << ", Titulo: " << Titulo[i] << endl;
+    }
+    
+    int idTarefa;
+    cout << "Digite o ID da tarefa que deseja editar: " << endl;
+    cin >> idTarefa;
+    
+    int posicaoTarefa = -1;
+    for (int i = 0; i < controle; i++){
+        if (ID[i] == idTarefa){
+            posicaoTarefa = i;
+            break;
+        }
+    }
+    
+    }
+    if (posicaoTarefa != -1){
+        // Permitir que o usuário edite os detalhes da tarefa
+        cout << "Editar tarefa ID: " << ID[posicaoTarefa] << endl;
+        cout << "Novo Título: ";
+        cin.ignore(); getline(cin, Titulo[posicaoTarefa]);
+        cout << "Nova Descrição: ";
+        cin.ignore(); getline(cin, Descricao[posicaoTarefa]);
+        cout << "Nova Data limite: ";
+        cin.ignore(); getline(cin, Data[posicaoTarefa]);
+        cout << "Novo Status: ";
+        cin >> Status[posicaoTarefa];
+        cout << "Tarefa editada com sucesso!" << endl;
+    } else {
+        cout << "ID de tarefa inválido." << endl;
+    }
+}
+}while (menu != 0);
+   
+   
+    
 return 0;
 }
